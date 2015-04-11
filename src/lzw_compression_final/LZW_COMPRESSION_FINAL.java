ad13f294161d67cf662aa24c0823b3402a6596da
Hasan Hafiz Pasha <mac_pasha@ovi.com>
2015-04-11 22:17:32 +0600

    Compression Complete

---
+++ src/lzw_compression_final/LZW_COMPRESSION_FINAL.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lzw_compression_final;
+
+/**
+ *
+ * @author pasha
+ */
+public class LZW_COMPRESSION_FINAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        compression_input inputData =   new compression_input();
+        inputData.setVisible(true);
+    }
+    
+}
